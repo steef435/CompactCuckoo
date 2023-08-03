6fde39c3351da057bd0b254a0782e572f41367c7
DaanWoltgens <d.p.woltgens@student.tue.nl>
2023-08-03 19:25:30 +0200

    Add echo to script

--- run_cc_tests.sh
+++ run_cc_tests.sh
@@ -4,7 +4,7 @@
 search_dir=new_models
 for entry in "$search_dir"/*.txt
 do
-	echo "reserve ./main benchspeed 21           9               1               1             1           0     1           1 nocleary $entry"
+	echo "reserve ./main benchspeed  21           9               1               1             100           0     1           1 nocleary $entry"
 	reserve ./main benchspeed 21           9               1               1             100           0     1           1 nocleary "$entry"
 done
 #Run the random dataset
